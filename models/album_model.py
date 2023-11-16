7a5118905ca8a897ef46072fcdd616d0524c2fc3
tphill <tphill760@gmail.com>
2023-11-16 18:47:41 +0000

    album model

---
+++ models/album_model.py
@@ -0,0 +1,18 @@
+from typing import List
+from typing import Optional
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Text, Table, Numeric
+
+class Base(DeclarativeBase):
+    pass
+
+class Albums(Base):
+    __tablename__ = "Albums"
+
+    id: Mapped[int] = mapped_column(primary_key = True, index = True, autoincrement="True")
+    name: Mapped[str] = Column(String, default="title")
+    artist_id: Mapped[str] = Column(String, default="duration")
+    release_date: Mapped[int] = Column(Integer, default="release date")
+
+    def __repr__(self) -> str:
+        return f"Albums(id={self.id!r}, name={self.name!r}, artist_id={self.artist_id!r}, release_date={self.release_date!r})"
---
+++ schemas/album_schema.py
@@ -0,0 +1,12 @@
+from typing import Optional
+from pydantic import BaseModel, EmailStr
+from models.song_model import Base
+
+class Base(BaseModel):
+    pass
+
+class AlbumsSchema(Base):
+    id: int
+    name: str | None
+    artist_id: str | None
+    release_date: int | None
